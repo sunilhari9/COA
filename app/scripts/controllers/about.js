eab48bf5323b940f759437575e06b8e38918e25a
Suneel Manyam <hmanyam@pkrmsezdc.com>
2018-07-25 15:13:57 +0530

    Init Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the coaApp
+ */
+angular.module('coaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the coaApp
+ */
+angular.module('coaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
