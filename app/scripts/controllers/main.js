f68f580c162de14b0d3dda32ffb4114605e75efb
murali <muralikolusu1219@gmail.com>
2018-07-31 16:25:00 +0530

    PDF maker intial commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /**
  * @ngdoc function
  * @name coaApp.controller:MainCtrl
@@ -7,39 +5,37 @@
  * # MainCtrl
  * Controller of the coaApp
  */
-app.controller('MainCtrl', function ($scope,$rootScope,$http) {
-    $scope.checkBoxOptions = 
-     {
-         "Item":"Item / Type",
-         "UnitPricing":"Unit Pricing",
-         "MFRName":"MFR NAME",
-         "MFRPart":"MFR PART #",
-         "ShippingPoint":"Shipping Point",
-         "CustomerItem":"Customer Item Sort",
-         "Tax":"Tax or no TAX",
-        "ThumbnailImages":"Thumbnail images",
-        "link":"Spec Sheet URL link",
-        "Country":"Country of Origin",
-        "LeadTime":"Lead Time",
-        "LongDescription":"Long Description",
-        "UNSPCS":"UNSPCS"
-     };
-    $rootScope.documentNumber="88882322";
-    $rootScope.customername="Suneel Manyam";
-    $rootScope.shipTo="234815";
-  });
+app.controller('MainCtrl', function ($scope, $rootScope, $http, $document) {
+    $scope.checkBoxOptions = {
+        "Item": "Item / Type",
+        "UnitPricing": "Unit Pricing",
+        "MFRName": "MFR NAME",
+        "MFRPart": "MFR PART #",
+        "ShippingPoint": "Shipping Point",
+        "CustomerItem": "Customer Item Sort",
+        "Tax": "Tax or no TAX",
+        "ThumbnailImages": "Thumbnail images",
+        "link": "Spec Sheet URL link",
+        "Country": "Country of Origin",
+        "LeadTime": "Lead Time",
+        "LongDescription": "Long Description",
+        "UNSPCS": "UNSPCS"
+    };
+    $rootScope.documentNumber = "88882322";
+    $rootScope.customername = "Suneel Manyam";
+    $rootScope.shipTo = "234815";
 
+    $scope.save = function () {
+        $scope.albumNameArray = $scope.albums.filter(function (album) {
+            return album.selected;
+        });
+    };
+    $scope.generatePDF = function () {
+      var docDefinition = {
+            content: 'This is an sample PDF printed with pdfMake'
+        };
+        pdfMake.createPdf(docDefinition).open();
 
+    };
 
-
-
-
-
-
-
-
-
-
-
-
- 
+});
